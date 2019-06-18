ff251a909e18627d0b759b17de2133d91805d1e6
Toron Seigler <toronseigler9@gmail.com>
2019-06-18 20:42:07 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,37 @@
 def create_an_empty_array
-  
+  []
 end
 
-def create_an_array
-  
+def create_an_array = ["bulldog", "terrier", "poodle", "pitbull"]
+
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array = ["pencil", "laptop", "desk", "pen"]
+  add_element_to_end_of_array<< 'eraser'
 end
 
 def add_element_to_start_of_array(array, element)
-  
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
 
 def update_element_from_index(array, index_number, element)
